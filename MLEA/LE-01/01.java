21b51c58b857ba5a7de382fb104f8d0b81a38cf3
Vinicius Soares <62343620+vncs-srs@users.noreply.github.com>
2023-08-15 13:31:59 -0300

    Criação MLEA.

---
+++ MLEA/LE-01/01.java
@@ -0,0 +1,27 @@
+//Escreva um modelo de classe para representar uma lâmpada que está `a venda em um supermercado.
+
+//Nome da classe/objeto
+public class Lampada
+{
+    // atributos
+    private double estadoDalampada;
+    private String mostraEstado;
+    
+    // metodos
+    public void acende(double estadoDalampada)
+    {
+        
+    }
+    public void apaga(double estadoDalampada)
+    {
+
+    }
+        public String mostraEstado()
+        {
+            return mostraEstado;
+        }
+    public void imprimirEstdadoLampada("Estado: " +  mostraEstado);
+    {
+        System.out.println();
+    }
+}
