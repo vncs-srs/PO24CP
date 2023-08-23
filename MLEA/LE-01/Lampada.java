56dd0ae23c4d86fb5b51fd9b23a326ec54241d63
Vinicius Soares <62343620+vncs-srs@users.noreply.github.com>
2023-08-23 13:07:24 -0300

    Exercício

---
+++ MLEA/LE-01/Lampada.java
@@ -0,0 +1,20 @@
+//Escreva um modelo de classe para representar uma lâmpada que está a venda em um supermercado.
+
+//Nome da classe/objeto
+public class Lampada
+{
+    // atributos
+
+    private double Potencia;
+    private String Marca;
+    private String Modelo;
+    private double Preco;
+    
+    // metodos
+
+    public void vendaLampada()
+    {
+        
+    }
+
+}
